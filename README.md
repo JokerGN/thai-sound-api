a0547fbf45015cb0ba2c41383daf8efaab68e7c7
JokerGN <chote1234@hotmail.co.th>
2017-12-18 18:01:26 +0700

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# thai-sound-api" 
---
+++ index.js
@@ -0,0 +1,19 @@
+import Koa from 'koa'
+import Router from 'koa-router'
+import bodyParser from 'koa-bodyparser'
+import cors from 'kcors'
+import 'babel-polyfill'
+import 'dotenv/config'
+
+const app = new Koa()
+const router = new Router({prefix: process.env.BASE_URI})
+
+app.use(bodyParser())
+app.use(cors())
+
+
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+console.log('App listen at PORT '+process.env.PORT)
+const server = app.listen(process.env.PORT || 3001)
