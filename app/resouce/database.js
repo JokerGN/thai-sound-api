6781c8184524632c13e42e8d9807ed0f5ac488c2
JokerGN <chote1234@hotmail.co.th>
2018-01-08 15:45:40 +0700

    Replace babel-polyfill with babel-register

--- app/resouce/database.js
+++ app/resouce/database.js
@@ -1,5 +1,4 @@
 import Sequelize from 'sequelize'
-import 'babel-polyfill'
 
 export const database = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
   host: process.env.DB_HOST,
