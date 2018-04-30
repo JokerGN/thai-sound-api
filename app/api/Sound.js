c076d43b6c9b72cca9114ee004d3bceb0c8ebeb5
JokerGN <chote1234@hotmail.co.th>
2018-05-01 01:59:52 +0700

    Add pagination for serach sound endpoint

--- app/api/Sound.js
+++ app/api/Sound.js
@@ -110,7 +110,9 @@ Sound.post('/delete_sound', async function (context, next) {
 Sound.post('/search_sound', async function (context, next) {
   let data = context.request.body
   let scope = {
-    scope: 'sounddetail'
+    scope: 'sounddetail',
+    offset: parseInt(data.offset),
+    limit: parseInt(data.limit)
   }
   let where = {
     typeId: data.typeId,
